7bf377cd3cdd14f249a303ee90705152befde7a4
mannya02 <msoni_be23@thapar.edu>
2026-03-01 23:27:29 +0530

    Update q3_serial.c

--- LAB5/Q3/q3_serial.c
+++ LAB5/Q3/q3_serial.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+#include <time.h>
 
+#define N 500000000LL
+
+int main() {
+    double multiplier = 2.0;
+    volatile double sum = 0.0;
+    double b = 2.0 * multiplier;
+
+    clock_t start = clock();
+    for (long long i = 0; i < N; i++) sum += 1.0 * b;
+    clock_t end = clock();
+
+    printf("Q3_SERIAL TIME=%f DOT=%0.3f\n",
+           (double)(end - start) / CLOCKS_PER_SEC, (double)sum);
+    return 0;
+}
